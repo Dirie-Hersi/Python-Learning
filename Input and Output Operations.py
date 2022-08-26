d8129f6b763199712cea120839fa36971277e6c3
Dirie-Hersi <dnhersi@gmail.com>
2022-08-26 01:38:22 +0000

    Added Input

---
+++ Input and Output Operations.py
@@ -0,0 +1,24 @@
+
+#Typcasting
+
+print(float(1))
+
+print(int(1.3))
+
+print(str(1))
+
+print(int('1'))
+
+print(bool(1))
+
+print(bool(0))
+
+#Input
+
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+age = int(input("How old are you today? "))
+
+print(name + " is " + str(age) + " years old and loves the color " + color)
+
+
