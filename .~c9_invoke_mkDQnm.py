4d1d2bcc5e7a417fcf126774d7d64b082fd45f6b
Dirie-Hersi <dnhersi@gmail.com>
2022-08-27 19:30:10 +0000

    More updates to Lists

---
+++ .~c9_invoke_mkDQnm.py
@@ -0,0 +1,20 @@
+#Basic List
+my_list = [1,2,3,4,5]
+print(my_list)
+print(len(my_list))
+
+#List with mixed types
+other_list = ['a', 1, 2.0, False]
+print(other_list)
+print(len(other_list))
+
+#Use slicing to get subsection of a list
+print(my_list[0:2])
+print(other_list[0::2])
+
+#Lists are mutable and can be modified
+my_list[0] = 'a'
+print(my_list)
+
+#Concatenate List
+my_list + [8,9,10]
