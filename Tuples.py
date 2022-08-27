1fd1089b1a7e4063d0867ed392196b45e77f1dea
Dirie-Hersi <dnhersi@gmail.com>
2022-08-27 22:04:54 +0000

    final tuple update

--- Tuples.py
+++ Tuples.py
@@ -8,3 +8,13 @@ print(my_second_tuple)
 
 x, y, z = my_tuple
 print(x,y,z)
+
+#List in a Tuple
+
+my_list = [1,2,3]
+my_tuple = (my_list, 1)
+print(my_tuple)
+
+#Tuple in a List
+other_list = [1,2, my_tuple]
+print(other_list)
