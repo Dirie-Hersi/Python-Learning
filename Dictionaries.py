cc881e4c31f5351a5252f47de33bcca6258e4847
Dirie-Hersi <dnhersi@gmail.com>
2022-08-28 04:19:10 +0000

    updated dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,20 @@
+#Basic dictionary
+ages = {'john': 29, 'alex' : 40, 'joe': 33}
+print(ages)
+
+#Getting value from key
+print(ages['john'])
+
+#Changing value
+ages['alex'] = 44
+print(ages)
+
+#Removing item
+del ages['john']
+print(ages)
+
+#keys method- list of dict keys
+print(ages.keys())
+
+#values method- list of dict value
+print(ages.values())
