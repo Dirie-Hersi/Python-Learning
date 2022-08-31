4d8e7c5e8e6c8382d8b1cfac060296a111a2e375
Dirie-Hersi <dnhersi@gmail.com>
2022-08-31 03:12:45 +0000

    Added Looping

---
+++ looping.py
@@ -0,0 +1,19 @@
+count = 1
+while count <= 4:
+    print("Looping!")
+    count +=1
+    
+#List    
+colors = ['blue', 'green', 'red', 'purple']
+for color in colors:
+    print(color)
+
+#Tuple
+point = (1,2,3)
+for value in point:
+    print(value)
+    
+#Dictionary
+ages = {'kevin': 59, 'bob': 40, 'kayla': 21}
+for key in ages:
+    print(key)
